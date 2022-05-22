caa5feedd7b79b02066b9ab9963a4cb8aec23332
Devin Jhu <devin.jhu@mths.ca>
2022-05-22 20:00:38 +0000

    code

--- numberGame.cpp
+++ numberGame.cpp
@@ -5,34 +5,52 @@
 // The celcius to fahrenheit calculator
 
 #include <iostream>
-#include <string>
+#include <random>
+#include <array>
 
 
-float fahrenheit() {
-    // this function converts celsius to fahrenheit
-    std::string celsiusString;
-    float celsius;
-    float fahrenheit;
+int largest(std::array<int, 10> numberArray) {
+    // this function finds the highest number in the num_array
+    int largestNumber = numberArray[0];
 
-    // input
-    std::cout << "enter temperature (celcius): ";
-    std::cin >> celsiusString;
-
-    // process & output
-    try {
-        celsius = std::stoi(celsiusString);
-
-        fahrenheit = ((9 * celsius) / 5) + 32;
-
-        std::cout << "\n"<< celsius <<"°C is also "<< fahrenheit << "°F ";
-    } catch (std::invalid_argument) {
-    std::cout << "not a temperature" << std::endl;
+    for (int counter = 0; counter < numberArray.size(); counter++) {
+        if (numberArray[counter] > largestNumber) {
+            largestNumber = numberArray[counter];
+        }
     }
+
+    return largestNumber;
 }
 
-int main() {
-    // this function calls the function
 
-    // call conversion
-    fahrenheit();
+int main() {
+    // this function generates 10 random numbers, calls a function,
+    // and outputs the highest one
+
+    std::array<int, 10> numberArray;
+    int randomNumber;
+    int largestNumber;
+
+    // random seed
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne twister
+    std::uniform_int_distribution<int> idist(0, 100);  // random from 0 - 100
+
+    std::cout <<
+    "This program generates 10 random numbers and finds the highest one.\n"
+    << std::endl;
+
+    // process and output
+    for (int counter = 0; counter < 10; counter++) {
+        randomNumber = idist(rgen);
+        numberArray[counter] = randomNumber;
+        std::cout << "Random number: " << randomNumber << std::endl;
+    }
+
+    largestNumber = largest(numberArray);
+
+    // output
+    std::cout << "The largest number is " << largestNumber << "." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
 }
